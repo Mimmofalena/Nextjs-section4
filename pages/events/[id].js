2ad401e7b80a31941991d198dbb6bbabf70c78c8
Domenico <domenico.cucinotta@iscsconsulting.com>
2022-12-03 19:12:04 +0100

    reset to initial project

--- pages/events/[id].js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Id = () => {
-  return <div>Id Page</div>;
-};
-
-export default Id;
--- pages/events/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Events = () => {
-  return <div>Main Events Page</div>;
-};
-
-export default Events;
